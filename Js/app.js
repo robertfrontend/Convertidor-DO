76aa21e97b9804da75d09a44a456a75631bc1612
Robert Romero Medina <enanow41romero@gmail.com>
2020-06-10 17:00:08 -0400

    agg: peso dominicano a dolar y biseversa

--- Js/app.js
+++ Js/app.js
@@ -5,19 +5,19 @@ var inputVal = document.getElementById('inputValor')
 var inputResultado = document.getElementById('result')
 var formato = document.getElementById('formato')
 
-var dolar = 57;
-var euro = 60;
+const dolar = 57;
+const euro = 60;
 
-
+// Convertir Dolar
 btnDolar.addEventListener('click', () => {
     console.log('Dolar');
     
     inputVal.classList = 'form-control dolar';
     inputResultado.style.display = 'block';
     formato.innerHTML = `
-        Dolar 👉 Peso Dominicano
+        Dolar <span id="invertirUs" >👉</span> Peso Dominicano
     `;
-
+    
     inputVal.addEventListener('keyup', () => {
         var sumatoria = inputVal.value * dolar;
         inputResultado.innerHTML = `
@@ -25,15 +25,38 @@ btnDolar.addEventListener('click', () => {
         `;
     })
 
+    var invertirUs = document.getElementById('invertirUs')
+    invertirUs.style.cursor = 'pointer'
+    invertirUs.addEventListener('click', inverDolar)
+
 });
 
+function inverDolar() {
+    const psDominicano = 0.017;
+
+    formato.innerHTML = `
+        Peso Dominicano <span id="invertirUs" >👉</span> Dolar
+    `;
+
+    invertirUs.innerHTML = '👈'
+
+    inputVal.addEventListener('keyup', () => {
+        var sumatoria = inputVal.value * psDominicano
+        inputResultado.innerHTML = `
+        $${Intl.NumberFormat().format(sumatoria) }
+    `;
+    })
+
+}
+
+// Convertir Euro
 btnEu.addEventListener('click', () => {
     console.log('euro');
 
     inputVal.classList = 'form-control euro';
     inputResultado.style.display = 'block';
     formato.innerHTML = `
-        Euro 👉 Peso Dominicano
+        Euro <span id="invertirEu" >👉</span> Peso Dominicano
     `;
 
     inputVal.addEventListener('keyup', () => {
@@ -44,4 +67,25 @@ btnEu.addEventListener('click', () => {
             $${Intl.NumberFormat().format(sumatoria) }
         `;
     })
+
+    var invertirEu = document.getElementById('invertirEu')
+    invertirEu.style.cursor = 'pointer'
+    invertirEu.addEventListener('click', invertirEuro)
 });
+
+function invertirEuro() {
+    const psDominicano = 0.015;
+
+    formato.innerHTML = `
+        Peso Dominicano <span id="invertirEu" >👉</span> Euro
+    `;
+
+    invertirEu.innerHTML = '👈'
+
+    inputVal.addEventListener('keyup', () => {
+        var sumatoria = inputVal.value * psDominicano
+        inputResultado.innerHTML = `
+        $${Intl.NumberFormat().format(sumatoria) }
+    `;
+    })
+}
